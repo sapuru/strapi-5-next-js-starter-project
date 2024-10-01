a9d60b23cf7a4178fb11ac9e9e9a4aaf27093e1f
aymara <aymarasamudio@gmail.com>
2024-10-01 10:34:40 -0300

    strapi ver 1

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "http",
-        hostname: "localhost",
+        hostname: "127.0.0.1",
         port: "1337",
         pathname: "/uploads/**/*",
       },
